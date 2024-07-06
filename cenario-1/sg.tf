fde4c2b3946380a08271d3229861ec2fcaaafb55
noronh4 <noronha.rafael@hotmail.com>
2024-07-06 00:37:14 -0300

    feat: add security groups(ssh and icmp)

---
+++ cenario-1/sg.tf
@@ -0,0 +1,58 @@
+resource "aws_security_group" "allow_ssh_icmp_public" {
+  name = "allow_ssh_icmp_public"
+  description = "Allow SSH and ICMP inbound traffic to all"
+  vpc_id = aws_vpc.VPC-CENARIO-1.id
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+
+  depends_on = [ aws_vpc.VPC-CENARIO-1 ]
+}
+
+resource "aws_security_group" "allow_ssh_icmp_private" {
+  name = "allow_ssh_icmp_private"
+  description = "Allow SSH only to the public subnet and ICMP inbound traffic"
+  vpc_id = aws_vpc.VPC-CENARIO-1.id
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+
+  depends_on = [ aws_vpc.VPC-CENARIO-1 ]
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_public" {
+  security_group_id = aws_security_group.allow_ssh_icmp_public.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_private" {
+  security_group_id = aws_security_group.allow_ssh_icmp_private.id
+  cidr_ipv4         = aws_subnet.SUBNET-CENARIO-1-PUBLIC.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = -1
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp_ipv4_public" {
+  security_group_id = aws_security_group.allow_ssh_icmp_public.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = -1
+  ip_protocol       = "icmp"
+  to_port           = -1
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_public" {
+  security_group_id = aws_security_group.allow_ssh_icmp_public.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_private" {
+  security_group_id = aws_security_group.allow_ssh_icmp_private.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
