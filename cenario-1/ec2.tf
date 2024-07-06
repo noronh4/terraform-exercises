a925078c9259e92103bb003e9b6c39658743ea1c
noronh4 <noronha.rafael@hotmail.com>
2024-07-06 00:26:50 -0300

    feat: add intances and ebs volumes

---
+++ cenario-1/ec2.tf
@@ -0,0 +1,58 @@
+resource "aws_instance" "ec2_public" {
+  ami = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.SUBNET-CENARIO-1-PUBLIC
+  security_groups = [ "aws_security_group.allow_ssh_icmp_public.id" ]
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+  
+  depends_on = [ aws_vpc.VPC-CENARIO-1 ]
+
+}
+
+resource "aws_instance" "ec2_private" {
+  ami = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.SUBNET-CENARIO-1-PRIVATE
+  security_groups = [ "aws_security_group.allow_ssh_icmp_private.id" ]
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+
+  depends_on = [ aws_vpc.VPC-CENARIO-1 ]
+
+}
+
+resource "aws_ebs_volume" "ebs_ec2_public" {
+  availability_zone = "us-east-1a"
+  size              = 2
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+}
+
+resource "aws_ebs_volume" "ebs_ec2_private" {
+  availability_zone = "us-east-1a"
+  size              = 2
+
+  tags = {
+    Name = "TERRAFORM-CENARIO-1"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att_ec2_public" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs_ec2_public.id
+  instance_id = aws_instance.ec2_public
+
+}
+
+resource "aws_volume_attachment" "ebs_att_ec2_private" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs_ec2_private
+  instance_id = aws_instance.ec2_private
+}
