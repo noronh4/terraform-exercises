4507ce66360e2f67883074c4b0a88e0e8f8328b1
noronh4 <noronha.rafael@hotmail.com>
2024-07-08 17:03:08 -0300

    fix: fixing rule allow_ssh_ipv4_private's port

--- cenario-1/sg.tf
+++ cenario-1/sg.tf
@@ -35,7 +35,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_private" {
   cidr_ipv4         = aws_subnet.SUBNET-CENARIO-1-PUBLIC.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = -1
+  to_port           = 22
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_icmp_ipv4_public" {
   security_group_id = aws_security_group.allow_ssh_icmp_public.id
