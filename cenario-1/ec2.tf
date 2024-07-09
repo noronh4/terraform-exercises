7efa59246256d85228fb52aa949f5aa4f05b89ef
noronh4 <noronha.rafael@hotmail.com>
2024-07-09 18:50:30 -0300

    chore: add a specific file to ebs

--- cenario-1/ec2.tf
+++ cenario-1/ec2.tf
@@ -70,14 +70,14 @@ resource "aws_ebs_volume" "ebs_ec2_private" {
 }
 
 resource "aws_volume_attachment" "ebs_att_ec2_public" {
-  device_name = "/dev/sdh"
+  device_name = "/dev/xvdh"
   volume_id   = aws_ebs_volume.ebs_ec2_public.id
   instance_id = aws_instance.ec2_public.id
 
 }
 
 resource "aws_volume_attachment" "ebs_att_ec2_private" {
-  device_name = "/dev/sdh"
+  device_name = "/dev/xvdh"
   volume_id   = aws_ebs_volume.ebs_ec2_private.id
   instance_id = aws_instance.ec2_private.id
 }
