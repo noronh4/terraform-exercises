4ce3af482d28e8e4e1338d1b6ae165d0016d532e
noronh4 <noronha.rafael@hotmail.com>
2024-07-11 16:12:39 -0300

    chore: update aws provider credentials

--- cenario-1/vpc.tf
+++ cenario-1/vpc.tf
@@ -8,7 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile = "default"
 
 }
 
