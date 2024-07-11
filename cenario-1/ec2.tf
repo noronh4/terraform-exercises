d68532b93f695bdc5a0529ed35c43ca617364259
noronh4 <noronha.rafael@hotmail.com>
2024-07-10 23:59:50 -0300

    fix: remove ebs settings from ec2.tf file

--- cenario-1/ec2.tf
+++ cenario-1/ec2.tf
@@ -50,34 +50,3 @@ resource "aws_instance" "ec2_private" {
   depends_on = [ aws_vpc.VPC-CENARIO-1 ]
 
 }
-
-resource "aws_ebs_volume" "ebs_ec2_public" {
-  availability_zone = "us-east-1a"
-  size              = 2
-
-  tags = {
-    Name = "TERRAFORM-CENARIO-1"
-  }
-}
-
-resource "aws_ebs_volume" "ebs_ec2_private" {
-  availability_zone = "us-east-1b"
-  size              = 2
-
-  tags = {
-    Name = "TERRAFORM-CENARIO-1"
-  }
-}
-
-resource "aws_volume_attachment" "ebs_att_ec2_public" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs_ec2_public.id
-  instance_id = aws_instance.ec2_public.id
-
-}
-
-resource "aws_volume_attachment" "ebs_att_ec2_private" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs_ec2_private.id
-  instance_id = aws_instance.ec2_private.id
-}
