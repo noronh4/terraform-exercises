b913e55553b9a5bac320af04abb700c1b7aeb44c
noronh4 <noronha.rafael@hotmail.com>
2024-07-12 19:37:15 -0300

    chore: terraform fmt

--- cenario-2/main.tf
+++ cenario-2/main.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
   shared_config_files      = ["$HOME/.aws/config"]
   shared_credentials_files = ["$HOME/.aws/credentials"]
-  profile = "default"
+  profile                  = "default"
 
 }
