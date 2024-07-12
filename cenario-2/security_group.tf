525165bb59069f07465f2078344d994cf0a0e4b1
noronh4 <noronha.rafael@hotmail.com>
2024-07-12 19:41:51 -0300

    feat: add security group that will be used in ec2 launch template

---
+++ cenario-2/security_group.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "allow_http_https_ssh" {
+  name        = "allow_http_https_ssh"
+  description = "Allow HTTP and SSH inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name = "allow_http_https_ssh"
+    Env  = "cenario_2"
+  }
+
+  depends_on = [aws_vpc.vpc]
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_http_https_ssh.id
+  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_http_https_ssh.id
+  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_http_https_ssh.id
+  cidr_ipv6         = aws_vpc.vpc.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http_https_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
