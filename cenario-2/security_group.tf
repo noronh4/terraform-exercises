6700f7ef9c949297f373961178f20e387ecaabb7
noronh4 <noronha.rafael@hotmail.com>
2024-07-12 19:47:30 -0300

    fix: fix cidr parameter name to ipv4

--- cenario-2/security_group.tf
+++ cenario-2/security_group.tf
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.allow_http_https_ssh.id
-  cidr_ipv6         = aws_vpc.vpc.cidr_block
+  cidr_ipv4         = aws_vpc.vpc.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
