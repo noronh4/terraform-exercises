58abb9614c53f8a9366212a553be807a81052232
noronh4 <noronha.rafael@hotmail.com>
2024-07-12 21:13:12 -0300

    fix: fix security groups cidr_ipv4 paramenter in rules allow_http, allow_https and allow_ssh

--- cenario-2/security_group.tf
+++ cenario-2/security_group.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_http_https_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_http_https_ssh.id
-  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -21,7 +21,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.allow_http_https_ssh.id
-  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.allow_http_https_ssh.id
-  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
