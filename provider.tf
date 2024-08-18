d842e9ee34aff70cbb19ba090556758eb83f0557
noronh4 <noronha.rafael@hotmail.com>
2024-08-18 00:13:44 -0300

    feat: aws provider config

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile                  = "default"
+
+}
+
