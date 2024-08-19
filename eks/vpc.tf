c10f76bc361836d5643c37f91dbf07d64d081456
noronh4 <noronha.rafael@hotmail.com>
2024-08-19 20:44:31 -0300

    fix: add subnet's AZ dynamically

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -10,11 +10,13 @@ resource "aws_vpc" "eks_vpc" {
   }
 }
 
+data "aws_region" "current" {}
+
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
 
   tags = {
     Name    = "eks_subnet_public_1a"
@@ -26,7 +28,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "${data.aws_region.current.name}b"
 
   tags = {
     Name    = "eks_subnet_private_1b"
