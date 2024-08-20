146cd6c440193ce30b49b633625be1b714d595eb
noronh4 <noronha.rafael@hotmail.com>
2024-08-19 21:19:43 -0300

    fix: creating a specific file for private and public subnets

---
+++ eks/private-subnets.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "eks_subnet_private_1a"
+
+  }
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "eks_subnet_private_1b"
+
+  }
+}
--- eks/vpc.tf
+++ eks/vpc.tf
@@ -10,30 +10,3 @@ resource "aws_vpc" "eks_vpc" {
     "kubernetes.io/role/elb" = 1
   }
 }
-
-data "aws_region" "current" {}
-
-resource "aws_subnet" "eks_subnet_public_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${data.aws_region.current.name}a"
-
-  tags = {
-    Name    = "eks_subnet_public_1a"
-    Billing = "eks_billing"
-
-  }
-}
-
-resource "aws_subnet" "eks_subnet_private_1b" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "${data.aws_region.current.name}b"
-
-  tags = {
-    Name                              = "eks_subnet_private_1b"
-    Billing                           = "eks-billing"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
