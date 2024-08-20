b94f16610e2cff82b5e718a24c3b471403801a44
noronh4 <noronha.rafael@hotmail.com>
2024-08-19 23:51:18 -0300

    feat: creating and attach public route table

---
+++ eks/public_route_table.tf
@@ -0,0 +1,31 @@
+resource "aws_route_table" "route_table_public" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "var.project_name}-route_table_public"
+    }
+  )
+
+}
+
+#route-table-association
+resource "aws_route_table_association" "route_table_association_subnet_public_1" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.route_table_public.id
+
+
+}
+
+#route-table-association
+resource "aws_route_table_association" "route_table_association_subnet_public_2" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.route_table_public.id
+
+}
